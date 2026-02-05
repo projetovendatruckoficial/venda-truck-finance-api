8555157180d4a680c824f53b1793fdd4e3273b08
Cade Auto Peças <projetovendatruckoficial@gmail.com>
2026-02-05 10:48:57 -0300

    novo commit

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel' || trim($slot) === 'Venda Truck Finance')
-<img src="https://vendatruck.com.br/wp-content/uploads/2023/01/logo-site-vetor.svg" class="logo" alt="{{ config('app.name') }}" style="height: 80px; width: 280px;">
+<img src="https://vendatruck.com.br/wp-content/uploads/2023/01/logo-site-vetor.svg" class="logo" alt="{{ config('app.name') }}" style="height: 90px; width: 280px;">
 @else
 {{ $slot }}
 @endif
